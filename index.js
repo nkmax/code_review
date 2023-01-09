1634f960d2bff6705705d3af982f09aeae769887
nkmax <natasha_ivanishko@mail.ru>
2023-01-09 08:47:02 +0000

    init commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const btn = document.querySelector(".start-game");
+
+function month() {
+  const n = prompt("Введите число от 1-12");
+
+  if (n == 1) {
+    return "Январь";
+  } else if (n == 2) {
+    return "Февраль";
+  } else if (n == 3) {
+    return "Март";
+  } else if (n == 4) {
+    return "Апрель";
+  } else if (n == 5) {
+    return "Май";
+  } else if (n == 6) {
+    return "Июнь";
+  } else if (n == 7) {
+    return "Июль";
+  } else if (n == 8) {
+    return "Август";
+  } else if (n == 9) {
+    return "Сентябрь";
+  } else if (n == 10) {
+    return "Октябрь";
+  } else if (n == 11) {
+    return "Ноябрь";
+  } else if (n == 12) {
+    return "Декабрь";
+  }
+
+  console.log('Месяц не найден');
+}
